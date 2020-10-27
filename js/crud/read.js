5370b10cacb1a257bd347bf418aed4c33ad81e4a
Z <zacharyk.ishmael@gmail.com>
2020-10-27 16:00:22 +0000

    read.js now implements the directory and file name reading on document load

--- js/crud/read.js
+++ js/crud/read.js
@@ -1,14 +1,27 @@
 //Read tag directories on page/document load
 $(document).ready(function() {
-    window.CRUD.readDirectories('all');//reads tag directories
-
+    window.CRUD.readDirectories();//reads tag directories
+    //return topics to html side panel
     window.CRUD.readDResponse(function(dirHTML) {
         document.querySelector('#topics').innerHTML = dirHTML;
+    });
+
+
+    //
+    window.CRUD.readDirectoryFiles('all');
+    window.CRUD.readDFResponse(function(filesHTML) {
+        document.querySelector('#files').innerHTML = filesHTML;
     });
 });
 
 // an object with an event/trigger for reading from a file to begin
-window.CRUD.readDirectories('all');
-//function to request main process to read data
+
+// window.CRUD.readDirectories();
+
+// //function to request main process to read data
+// window.CRUD.readDFResponse()
+
+// window.CRUD.readDResponse()
+
 
 //function to recieve response from main process - successful or unsuccessful
