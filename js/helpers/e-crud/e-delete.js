efff6b7099395312e6cec3f00819ae9e48a0d620
Z <zacharyk.ishmael@gmail.com>
2020-10-29 14:46:30 +0000

    Wroked on e-delete.js and created directory.js

---
+++ js/helpers/e-crud/e-delete.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const directory = require('../directory');
+
+exports.delete = (event, filename) => deleteEntry(event, filename);
+
+
+function deleteEntry(event, filename) {
+    
+    path = directory.all + filename;
+    callback = function (error) {
+        if (error) {
+            console.log('Error deleting file:'+error);
+            channel = 'response-e-delete';
+            message = 'Error deleting file:'+filename;
+            event.reply(channel, message);
+            throw error;
+        }
+    };
+    fs.unlink(path, callback);
+}
