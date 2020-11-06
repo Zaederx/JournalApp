626e8beab0241e99975f6453e9588c4283229d0c
Z <zacharyk.ishmael@gmail.com>
2020-11-06 11:18:27 +0000

    Added Entry Field constants

--- js/crud/update.js
+++ js/crud/update.js
@@ -1,10 +1,11 @@
 const btn_update = document.querySelector('#e-update');
-
-
+const title = document.querySelector('#e-title');
+const body = $('#e-body');
+const tags = $('#e-tags');
 btn_update.onclick = function update(event) {
     //read entry
     entry = getECurrent();//from vars.js
-
+    
     //insert into fields
     
     //read fields for changes
