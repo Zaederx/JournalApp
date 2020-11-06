d844b5b6e4d852c7a722f7e931387470b77933f5
Z <zacharyk.ishmael@gmail.com>
2020-11-06 11:08:12 +0000

    Added e-update button const to update.js

---
+++ js/crud/update.js
@@ -0,0 +1,17 @@
+const btn_update = document.querySelector('#e-update');
+
+
+btn_update.onclick = function update(event) {
+    //read entry
+    entry = getECurrent();//from vars.js
+
+    //insert into fields
+    
+    //read fields for changes
+
+    //create entry of same name to replace original
+
+    //delete original?
+
+    //save new entry
+}
