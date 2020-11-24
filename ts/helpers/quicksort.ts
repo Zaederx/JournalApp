cfbe4fb2b0e6709cb629cce6dd85515edd8721fa
Z <zacharyk.ishmael@gmail.com>
2020-11-24 21:49:50 +0000

    Create quicksort.ts

---
+++ ts/helpers/quicksort.ts
@@ -0,0 +1,37 @@
+function swap(arr:number[], i:number, j:number) {
+    var x:number = arr[i];
+    arr[i] = arr[j];
+    arr[i] = x;
+}
+
+// arr = array of integers
+// s = start index = 0
+// e = end index
+//returns - partitionIndex = index of the partition variable after the partition is performed - required by quicksort
+function partition(arr:number[], s:number, e:number): number {
+    var a:number[] = arr;
+    var start:number = s;
+    var end:number = e;
+    var pivot:number = a[end];
+    var i: number = start - 1;
+
+    for(var j = start; j < end - 1; j++ ) {
+        if (a[j] <= pivot) {
+            i++;
+            swap(a, a[i], a[j]);
+        }
+    }
+    swap(a ,a[i+1], a[end]);
+    return i+1;
+}
+
+//repetedly calls partition around a pivot points in order to sort arr elements
+function quicksort(arr:number[], start:number, end:number):number[] {
+    var a:number[] = arr;
+    if (start < end){
+        var partitionIndex = partition(a, start, end);
+        quicksort(a,start,partitionIndex-1);
+        quicksort(a, partitionIndex+1, end);
+    }
+    return a;
+}
