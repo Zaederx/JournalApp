a1d6549106d2c0edce4fca0be4616c61421dfdfb
Z <zacharyk.ishmael@gmail.com>
2020-12-01 20:45:12 +0000

    e-read.ts - Entries now sorted from latest to earliest
    Additions:
    - addition of fetchBtime function
    
    Chnages:
    - Update to import method/strategy
    - readAllDirectories function - corrections and added type declarations
    - readDirFiles function - now implements sorting

--- ts/helpers/e-crud/e-read.ts
+++ ts/helpers/e-crud/e-read.ts
@@ -1,109 +1,137 @@
-const fs = require('fs');
-const dateSort = require('../algorithms/datesort');
-const {spawn} = require('child_process');
-const stat_birth = spawn('stat',['-f','%B']); //File Birthtime milliseconds
-// exports.readAllDirectories = (event) => readAllDirectories(event);
-// exports.readDirFiles =  (event, dir) => readDirFiles(event, dir);
-// exports.readSingleFile = (event, filename) => readSingleFile(event,filename);
+import * as fs from 'fs';
+import * as entrySort from '../algorithms/entrysort';
+import * as process from 'child_process';
+import {EntryDate} from '../../classes/EntryDate';
 
-
-
-
+/**
+ * Read all tag Directories.
+ * Used to fill tag directory side bar/panel
+ * @param event - An Electron Event
+ */
 export function readAllDirectories(event) {
-  var directory;
+  var directory:string[];
+  var dirHTML = '';
   try {
       //using readdirSync - blocks IO until the read is done - will try sending event reply only once directories are loaded
       directory = fs.readdirSync('tagDirs/');
-
+      var counter:number = 0;
+      directory.forEach( subdirectory => {
+        if (counter == 0) {
+            dirHTML += '<div class="active tag">'+subdirectory+'</div>\n';//must be active tag!
+            counter++;
+        }
+        else {
+            dirHTML += '<div>'+subdirectory+'</div>\n';
+        }
+        });
+        event.reply('response-d-read', dirHTML);
+  
   } catch (err) {
       console.error('Entry folder could not be read');
   }
-
-  var dirHTML = '';
-
-  var counter = 0;
-  var arr = [];
-  try {
-      directory.forEach( subdirectory => {
-      if (counter == 0) {
-          fetchBtime(subdirectory,arr);
-          counter++;
-      }
-      else {
-          fetchBtime(subdirectory, arr);
-      }
-      });
-      // event.reply('response-d-read', dirHTML);
-  } catch (err) {
-      console.error('Problem reading directories.');
-  }
-
-  var newArr = dateSort.sort(arr);//returns string[]
-
-  var i = 0;
-  newArr.forEach(date => {
-    if (i == 0) {
-      dirHTML += '<div class="active">'+date+'</div>\n';
-      i++;
-    } 
-    else {
-      dirHTML += '<div>'+date+'</div>\n';
-    }
-  });
-  event.reply('response-d-read', dirHTML);
+  
 }
 
-export function fetchBtime(subdirectory, arr) {
-  stat_birth.stdout.on('data', (btime) => {
-    arr.push([subdirectory,btime]);
-    console.log('stdout: ${btime}');
-  });
-  stat_birth.stderr.on('data', (err) => {
-    console.error('File Birthtime error:',err);
-  });
-  stat_birth.on('close', (code) => {
-    console.log('child process: "stat_birth" exited with code ${code}');
-  });
-}
 
+
+/**
+ * Read names of all the files in a directory.
+ * @param event Electron Event
+ * @param dir (path of) directory to be read
+ */
 export function readDirFiles(event,dir) {
     console.log('ipcMain: Reading new Entry - ' + dir);
     var directory;
     var filesHTML = '';
-      fs.readdir('tagDirs/'+ dir+'/', (error, files)=> {
-  
+    var arr:EntryDate[] = [];
+    var prefix:string = 'tagDirs/'+ dir+'/';
+      fs.readdir(prefix, (error, files)=> {
         if (error) {
           event.reply('Entry folder '+dir+' could not be read.');
           console.log('Entry folder could not be read.');
           return console.error(error);
         }
         else {
-          var count = 0;
-          files.forEach( file => {
-            if (count == 0) {
-              filesHTML += '<div class="active entry">'+file+'</div>\n';
-              count++;
-            } else {
-              filesHTML += '<div>'+file+'</div>\n';
+          
+          files.forEach( file => fetchBtime(prefix,file,arr));
+          var start:number = 0;
+          var end:number = arr.length-1;
+          var newArr = entrySort.sort(arr,start,end);
+
+          var i:number = 0;
+          newArr.forEach(entryDate => {
+            if (i == 0) {
+              filesHTML += '<div class="active entry">'+entryDate.name+'</div>\n';//class must be active entry!
+              i++;
+            } 
+            else {
+              filesHTML += '<div>'+entryDate.name+'</div>\n';
             }
           });
           event.reply('response-de-read', filesHTML);
-          
+         
         }
-  
       });
+  
 }
 
+/**
+ * 
+ * @param event 
+ * @param filename 
+ */
 export function readSingleFile(event,filename) {
-console.log('ipcMain: Reading file - '+filename);
-var dir = 'tagDirs/all/';
-fs.readFile(dir+filename, 'utf-8',(error, file) => {
+
+  console.log('ipcMain: Reading file - '+filename);
+
+  var dir = 'tagDirs/all/';
+
+  fs.readFile(dir+filename, 'utf-8',(error, file) => {
+
     if (error) {
-    event.reply('Entry '+file+' could not be read.');
-    console.error('Entry could not be read.');
-    } else {
-    event.reply('response-e-read', file);
-    console.info('File read successfully.');
+      event.reply('Entry '+file+' could not be read.');
+      console.error('Entry could not be read.');
+    } 
+
+    else {
+      event.reply('response-e-read', file);
+      console.info('File read successfully.');
     }
-})
+
+  })
 }
+
+/**
+ * Finds birthtime of a subdirectories.
+ * Once the birthtime is known, it creates an EntryDate
+ * and adds it to the given array.
+ * @param file name of the subdirectory - i.e. tagDirectory 
+ * @param arr an array of EntryDate
+ */
+function fetchBtime(prefix:string,file:string, arr:EntryDate[]) {
+  /**
+   * From ZSH manual under 'stat' - see zsh terminal command 'man stat':
+   * 
+   *  -f format
+               Display information using the specified format.  See the FORMATS
+               section for a description of valid formats.
+  
+   * -L      Use stat(2) instead of lstat(2).  The information reported by
+               stat will refer to the target of file, if file is a symbolic
+               link, and not to file itself.
+   */
+  
+    var stat_birth = process.spawnSync('stat',['-f','%B', '-L', prefix+file]);
+    console.log('output',stat_birth.output);
+    var bString:string = stat_birth.stdout;
+    var btime:number = Number(stat_birth.stdout);
+    var eDate:EntryDate = new EntryDate(file, btime);
+    arr.push(eDate);
+    console.log('bString',bString);
+    console.log('btime',btime);
+    var err:string = stat_birth.stderr;
+    console.error('File Birthtime error:',err);
+
+    var code:number|null = stat_birth.status;
+    console.log('child process: "stat_birth" exited with code',code);
+  }
