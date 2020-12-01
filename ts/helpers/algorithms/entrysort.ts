164ae27002b6d66840f5241202591251d089ef93
Z <zacharyk.ishmael@gmail.com>
2020-12-01 11:16:26 +0000

    Create entrysort.ts

---
+++ ts/helpers/algorithms/entrysort.ts
@@ -0,0 +1,60 @@
+/**
+ * Functions for performing sorting on biarrays where the secons *  column is a number to be sorted, and the first coulnm is an *   attribute of importance.
+ */
+
+
+/**
+ * 
+ * @param arr 
+ * @param i 
+ * @param j 
+ */
+function biswap(arr:EntryDate[], i:number, j:number) {
+    var x:EntryDate= arr[i];
+    arr[i] = arr[j];
+    arr[j] = x;
+}
+
+// arr = array of integers
+// s = start index = 0
+// e = end index
+//returns - partitionIndex = index of the partition variable after the partition is performed - required by quicksort
+/**
+ * 
+ * @param arr the array to be partitioned
+ * @param s where to start evaluating the array (usually 0 i.e. the start index)
+ * @param e where to end evaluating the array (usually array.length-1 i.e. the end index)
+ */
+function bipartition(arr:EntryDate[], s:number, e:number): number {
+    var a:EntryDate[] = arr;
+    var start:number = s;
+    var end:number = e;
+    var pivot:EntryDate = a[end];
+    var i: number = start - 1;//partion index
+
+    for(var j = start; j < end - 1; j++ ) {//must stop one less than end index
+        if (a[j].btime <= pivot.btime) {
+            i++;
+            biswap(a, i, j);
+        }
+    }
+    biswap(a ,i+1, end);//partition index value with pivotIndex value
+    return i+1;
+}
+
+//repetedly calls partition around a pivot points in order to sort arr elements
+
+/**
+ * @param arr array of EntryDates to sort
+ * @param start where the sorts from in the array
+ * @param end where the sorts end in the array
+ */
+export function sort(arr:EntryDate[], start:number, end:number):EntryDate[] {
+    var a:EntryDate[] = arr;
+    if (start < end){
+        var partitionIndex = bipartition(a, start, end);
+        sort(a,start,partitionIndex-1);
+        sort(a, partitionIndex+1, end);
+    }
+    return a;
+}
