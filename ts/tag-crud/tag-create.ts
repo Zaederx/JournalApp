631416c786f69a25d796c66083e6042c0faaf90e
Z <zacharyk.ishmael@gmail.com>
2021-01-21 17:15:47 +0000

    tag-create.ts - loadTagTable method added + anonymous function - remove unused parameter

--- ts/tag-crud/tag-create.ts
+++ ts/tag-crud/tag-create.ts
@@ -1,13 +1,27 @@
-const btn_addTag:HTMLElement|null = document.querySelector('#btn-submit');
+const btn_addTag:HTMLElement|null = document.querySelector('#t-create');
+const tagTableBody:HTMLTableElement|null = document.querySelector('#tag-table-body');
 
 //TODO Add a succesful message/alert box adding a new tag
 
 if(btn_addTag != null)
-btn_addTag.onclick = (event) => displayTagView();
-
+btn_addTag.onclick = () => displayTagView();
 
 
 function addTag(tagname:string) {
     //add tag folder to tagDirs
     window.tagCRUD.create(tagname);
+}
+
+
+/**
+ * Fills tag table with all tags.
+ */
+function loadTagTable() {
+    //ipcMessage - requests tag to be read
+    window.CRUD.readTags(); 
+    //wait for for ipc response -> load tag table
+    window.CRUD.readTResponse((tagsHTML:string) => {
+        if (tagTableBody != null)
+        tagTableBody.innerHTML = tagsHTML;
+    });
 }
