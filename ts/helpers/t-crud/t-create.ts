0ba5157fc7a75c2c15c65cf9462be55b494e4e6a
Z <zacharyk.ishmael@gmail.com>
2021-01-22 18:14:16 +0000

    t-create.ts - promises used to correct function behaviour

--- ts/helpers/t-crud/t-create.ts
+++ ts/helpers/t-crud/t-create.ts
@@ -1,27 +1,34 @@
 import * as fs from 'fs';
 
+
+
 /**
  * Creates a tag folder.
  * 
  * @param tagName name of a tag to be created
  */
-function createTag(tagName:string) :boolean {
+function createTag(tagName:string, event?:Electron.IpcMainEvent):Promise<boolean> {
     console.log('ipcMain: Creating new Tag -'+tagName);
     var directory:string = 'tagDirs/';
     var success:boolean;
-    fs.mkdir(directory+tagName, {recursive: false} , (error) => {
-        var message:string = '';
-        if (error) {
-            message = 'An error occured in saving the new tag:'+ tagName;
-            success = false;
-        }
-        else {
-            message = 'Tag saved successfully';
-            success = true;
-        }
-        return success;
-    })
-    return false;
+    var message:string = '';
+    return new Promise<boolean>((resolve,reject) => {
+        fs.mkdir(directory+tagName, {recursive: false} , (error) => {
+            if (error) {
+                message = 'An error occured in saving the new tag:'+ tagName;
+                console.error(message);
+                reject(false);
+            }
+            else {
+                message = 'Tag saved successfully';
+                console.log(message);
+                resolve(true);
+            }
+             event?.reply(message);
+        })
+     
+    }).catch((reject:boolean) => { return reject});
+    
 }
 
 /**
@@ -50,5 +57,18 @@ function failure(event:Electron.IpcMainEvent, tagName:string) {
  * @param tagNames all the tag names to be created
  */
 export function createTags(event:Electron.IpcMainEvent, tagNames:string[]) {
-    if (tagNames != null) tagNames.forEach((tagName) => createTag(tagName) == true ? success(event) : failure(event,tagName));
+    if (tagNames != null) tagNames.forEach((tagName) => createTag(tagName) ? success(event) : failure(event,tagName));
+}
+
+/**
+ * Used to respond to tag creation events.
+ * Called from main class to create tags.
+ * @param tagNames all the tag names to be created
+ */
+export function createTagsInvoke(tagNames:string[]):boolean {
+    var response = true;
+    //for each tag - if tag is not create - mark flag 'response' as false otherwise do nothing
+    if (tagNames != null) tagNames.forEach((tagName) => createTag(tagName).then( (result) => { result == true ? response = response : response = false}));
+    console.error(tagNames);//TODO REMOVE When no loger needed for debugging 
+    return response;
 }
