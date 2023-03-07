e37e583fd8b51cb4d6a130c08720d0b8a975f0df
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-03-07 22:25:31 +0100

    Update export.ts

--- ts/view/export.ts
+++ ts/view/export.ts
@@ -8,52 +8,31 @@ import { dialog } from 'electron';
 import { OpenDialogReturnValue } from "electron/main";
 
 //variables
-var btn_export_txt = document.querySelector('#btn_export_txt') as HTMLButtonElement;
-var btn_export_json = document.querySelector('#btn_export_json') as HTMLButtonElement;
-var btn_export_pdf = document.querySelector('#btn_export_pdf') as HTMLButtonElement;
+var btn_export_txt = document.querySelector('#btn-export-txt') as HTMLButtonElement;
+var btn_export_json = document.querySelector('#btn-export-json') as HTMLButtonElement;
+var btn_export_pdf = document.querySelector('#btn-export-pdf') as HTMLButtonElement;
 
 btn_export_txt ? btn_export_txt.onclick = () => export_txt() : console.warn('btn_export_txt is null')
-btn_export_json ? (btn_export_json.onclick = () => export_json()) : console.warn('btn_export_json is null')
-btn_export_pdf ? (btn_export_pdf.onclick = () => export_pdf()) : console.warn('btn_export_pdf is null')
+btn_export_json ? btn_export_json.onclick = () => export_json() : console.warn('btn_export_json is null')
+btn_export_pdf ? btn_export_pdf.onclick = () => export_pdf() : console.warn('btn_export_pdf is null')
 
 async function export_txt()
 {
     console.log("function export_txt called")
-    var dialogPath = await ipcRenderer.invoke('get-tag-directory-filepath')
-    //open dialog box
-    var promise:OpenDialogReturnValue = await dialog.showOpenDialog({ defaultPath: dialogPath, properties: ['openFile', 'multiSelections'] })
-
-    //if not exited
-    if (!promise.canceled)
-    {
-        //get filepaths of entries
-        var entriesArr:string[] = promise.filePaths
-        //send entries to be exported
-        ipcRenderer.emit('export-entries-txt', entriesArr)
-    }
-    
+    //send message to ipcMain to export entries
+    ipcRenderer.invoke('export-entries-txt')
 }
 
 
 
 async function export_json()
 {
-    var dialogPath = await ipcRenderer.invoke('get-tag-directory-filepath')
-    //open dialog box
-    var promise:OpenDialogReturnValue = await dialog.showOpenDialog({ defaultPath: dialogPath, properties: ['openFile', 'multiSelections'] })
-
-    //if not exited
-    if (!promise.canceled)
-    {
-        //get filepaths of entries
-        var entriesArr:string[] = promise.filePaths
-        //send entries to be exported
-        ipcRenderer.emit('export-entries-json', entriesArr)
-    }
+   
 }
 
 async function export_pdf()
 {
+    console.log('function export_pdf called')
     var dialogPath = await ipcRenderer.invoke('get-tag-directory-filepath')
     //open dialog box
     var promise:OpenDialogReturnValue = await dialog.showOpenDialog({ defaultPath: dialogPath, properties: ['openFile', 'multiSelections'] })
