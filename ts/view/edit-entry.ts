0d25c7908ffb322b5d9d6180cac10dc954b53cc8
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-04-05 00:20:20 +0200

    edit-entry.ts - fixed problem with editing entries + refactoring

--- ts/view/edit-entry.ts
+++ ts/view/edit-entry.ts
@@ -12,14 +12,16 @@ async function loadFragment()
     const tags_popup = await (await fetch('./fragments/tags-popup.html')).text()
     document.querySelector('#tags-popup')!.outerHTML = tags_popup
 }
-var promise = loadFragment()
+var fragmentsLoaded = loadFragment()
 //Fetch HTML Elements
 const messageDiv = document.querySelector('#message') as HTMLDivElement
 const title = document.querySelector('#entry-title') as HTMLDivElement
 const body = document.querySelector('#entry-body') as HTMLDivElement
 const tags = document.querySelector('#entry-tags') as HTMLDivElement
-window.onload = () => displayCurrentEntry(title,body,tags)//window.onload must be outside of the promise to work - misses onload event firing otherwise
-promise.then(() => {
+//display the current entry - ready to be edited
+displayCurrentEntry(title,body,tags)
+//once fragments have loaded
+fragmentsLoaded.then(() => {
 //Div Buttons
 const update_entry = document.querySelector('#update-entry') as HTMLDivElement
 const save_new_entry = document.querySelector('#save-new-entry') as HTMLDivElement
@@ -32,25 +34,21 @@ edit_entry_tags ? edit_entry_tags.onclick = () => toggleTagPopup(main,tagTableBo
 
 //Functions
 //@ts-ignore
-var entryTemp = new Entry({})
-window.onload = () => displayCurrentEntry(title,body,tags)
+// var entryTemp = new Entry({})
+// window.onload = () => displayCurrentEntry(title,body,tags)
 
 
 //******** Handling The Tag Pop ********** */
+//fetch elements
 var btn_tags_popup = document.querySelector('#edit-tags-popup') as HTMLDivElement
 var btn_add_tags = document.querySelector('#add-selected-tags') as HTMLDivElement
 var btn_remove_tags = document.querySelector('#remove-selected-tags') as HTMLDivElement
 var main = document.querySelector('#main-container') as HTMLDivElement
 var close_btn = document.querySelector('#close-btn') as HTMLDivElement
-
-
 var tagTableBody1 = document.querySelector('#tag-table-body') as HTMLTableElement
-
+//enable elements
 btn_tags_popup ? btn_tags_popup.onclick = () => toggleTagPopup(main,tagTableBody1) : console.log('btn_tags_popup is null')
-
 btn_add_tags ? btn_add_tags.onclick = () => addSelectedTagsToEntry(tags, tagTableBody1) : console.log('add_tags btn is null')
-
 btn_remove_tags ? btn_remove_tags.onclick = () => removeSelectedTags(tagTableBody1, title, body, tags) : console.warn('btn_remove_tags is null')
-
 close_btn ? close_btn.onclick = () => toggleTagPopup(main,tagTableBody1) : console.warn('popup close_btn is null')
 })
