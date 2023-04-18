953d56958f14e6df99ef4f8865b420f112402915
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-04-18 19:58:36 +0200

    Update isThereTheDirectory.ts

--- ts/fs-helpers/isThereTheDirectory.ts
+++ ts/fs-helpers/isThereTheDirectory.ts
@@ -3,16 +3,17 @@ import * as fs from 'fs'
  * Checks whether the current entry directory exists.
  * @return true or false
  */
-export default async function isThereTheDirectory(dir:string)
+export default async function isThereADirectory(dir:string)
 {
   var isThere = false
-  try {
+  try 
+  {
     var stat = await fs.promises.stat(dir)
+    return stat.isDirectory()
   } catch (error) {
     console.log(error)
     return isThere//false
   }
-  isThere = true
-  return isThere
+
   
 }
