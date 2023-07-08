7b68c76766ebbfd042d6f2f21154cb3020c0196d
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-07-08 12:48:06 +0200

    Update createEntryPage.ts

--- test/pageableObjects/createEntryPage.ts
+++ test/pageableObjects/createEntryPage.ts
@@ -8,27 +8,27 @@ export default class CreateEntryPage extends Page
     }
     public async clickEntryButton()
     {
-        return await browser.$('#create-entry')
+        return (await browser.$('#create-entry')).click()
     }
 
     public async clickAddTagsButton()
     {
-        return await browser.$('#btn-add-tags')
+        return (await browser.$('#btn-add-tags')).click()
     }
 
     public async clickAddSelectedTagsButton()
     {
-        return await browser.$('#add-selected-tags')
+        return (await browser.$('#add-selected-tags')).click()
     }
 
     public async clickTagsPopupCloseButton()
     {
-        return await browser.$('#close-btn')
+        return (await browser.$('#close-btn')).click()
     }
 
     public async clickTagTableRow(i:number)
     {
-        var table = await browser.$('#tag-table-body')
-        table.$('.tr')[i].click()
+        var table = await browser.$('#tag-table-body');
+        table.$$('.tr')[0].click()
     }
 }
