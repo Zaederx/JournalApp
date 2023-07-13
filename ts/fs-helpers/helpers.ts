87d731ddaa56689b3f7e037152f226aba0344faf
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-07-13 12:37:11 +0200

    Update helpers.ts

--- ts/fs-helpers/helpers.ts
+++ ts/fs-helpers/helpers.ts
@@ -1,6 +1,6 @@
 import isThereTheDirectory from "./isThereTheDirectory";
 import isThereACurrentEntry from "./isThereACurrentEntry";
 import createAllTagDirectory from "./createAllTagDirectory"
-import isThereAFile, { isThereAFileStrict }from "./isThereAFile";
+import { isThereAFile, isThereAFileStrict } from "./isThereAFile";
 import isThereASymbolicLink from './isThereASymbolicLink';
 export { isThereTheDirectory, isThereACurrentEntry, createAllTagDirectory, isThereAFile, isThereAFileStrict, isThereASymbolicLink}
