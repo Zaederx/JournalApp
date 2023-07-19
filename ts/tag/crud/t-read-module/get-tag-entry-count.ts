ba93328f0b6c561fa8a4db1f783dcee62461f0fd
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-07-19 14:41:53 +0200

    get-tag-entry-count.ts - ignores hidden files now

--- ts/tag/crud/t-read-module/get-tag-entry-count.ts
+++ ts/tag/crud/t-read-module/get-tag-entry-count.ts
@@ -1,5 +1,5 @@
 import paths from 'path'
-
+import { printFormatted } from '../../../other/printFormatted'
 import { readDir } from './read-dir'
 /**
  * Get a tally(count) of a tag directories entries.
@@ -7,7 +7,18 @@ import { readDir } from './read-dir'
  * @returns 
  */
 export async function getTagEntryCount(dir:string,tag:string):Promise<number> {
+    printFormatted('blue', 'function getTagEntryCount called')
     var path = paths.join(dir,tag)
-    var entries = await readDir(path)
+    var entriesAndHidden = await readDir(path)
+    var entries:string[] = []
+    entriesAndHidden.forEach((filename) =>
+    {
+        printFormatted('yellow', 'filename:',filename)
+        if (filename.charAt(0) != '.')//if not hidden file
+        {
+            entries.push(filename)
+        } 
+    })
+    printFormatted('green', 'entry count:', entries.length)
     return entries.length
 }
