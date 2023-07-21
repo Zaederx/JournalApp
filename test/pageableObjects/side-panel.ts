a6a72267a31ae9304764025bee67b1e9c1de4eac
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-07-21 18:06:11 +0200

    remove wdio and related files from project

--- test/pageableObjects/side-panel.ts
+++
@@ -1,9 +0,0 @@
-import browser from '@wdio/globals'
-export default class SidePanelComponent
-{
-    static compId = '#side-panel'
-    public static isDisplayed()
-    {
-        return browser.$(this.compId).isDisplayed();
-    }
-}
