a1ff858f60d9995a271b690746210bc3521eed4b
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-04-24 18:10:10 +0200

    change filename and update file

--- ts/other/create-window.ts
+++ ts/other/create-window.ts
@@ -1,5 +1,10 @@
 import { BrowserWindow } from 'electron'
-import pathsForWDIO from './paths-for-wdio'
+import pathsForAppBinaries from './paths-for-app-binaries'
+/**
+ * Creates a window for the app and returns it
+ * @param integration - whether to enable node integration
+ * @return BrowserWindow for the app
+ */
 export default async function createWindow(integration:boolean=true) {
 
     
@@ -25,10 +30,12 @@ export default async function createWindow(integration:boolean=true) {
   
     await window.webContents.executeJavaScript('localStorage.setItem("inDialog","false")')
   
-    if (process.env.NODE_ENV === 'test-main') {
+    //Note:don't think it matters but it was... 
+    // if (process.env.NODE_ENV === 'test-main') {
+    if (process.env.NODE_ENV === 'test') {
       //produce paths for wdio
       window.webContents.openDevTools();
-      pathsForWDIO()
+      pathsForAppBinaries()
     }
       
     
