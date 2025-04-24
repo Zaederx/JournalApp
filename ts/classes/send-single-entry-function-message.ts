6d4f037b3878d5fdabc0d556594f04fb1a55a32f
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-04-24 21:58:36 +0200

    Update send-single-entry-function-message.ts

--- ts/classes/send-single-entry-function-message.ts
+++ ts/classes/send-single-entry-function-message.ts
@@ -1,6 +1,15 @@
+/**
+ * Class for representing messages sent from the 
+ * `sendSingleEntry` function in `ts/entry/crud/e-read-module/append-entries.ts`
+ */
 export default class  SendSingleEntryFunctionMessage{
     entryFilename:string
-    firstEntry:boolean
+    /**
+     * Whether the entry is the first entry to be sent.
+     * Important for working out whether or not to 
+     * clear entries on the side panel. (if it's the first entry, then the sidepanel needs to be cleared). See nav.ts
+     */
+    firstEntry:boolean 
     constructor(entryFilename:string,firstEntry:boolean) 
     {
         this.entryFilename = entryFilename
