a09d7f11fe73ff8e0b50474a867fec12d2a04044
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-04-27 15:57:58 +0200

    Update append-entries-tags.ts

--- ts/view/append/append-entries-tags.ts
+++ ts/view/append/append-entries-tags.ts
@@ -5,7 +5,11 @@ import { printFormatted } from '../../other/printFormatted'
 import paths from 'path'
 
 /**
- * Append entries and tags.
+ * Sends entries and tags to be added to the entry list
+ * and tag list on the frontend.
+ * Does this by running the script `append.js` (`append.ts`)
+ * `append.js` calls the functions `appendEntries` and
+ * `appendTags` which both 
  * @param event 
  * @param allEntries path to 'all' entries tag folder
  * @param tagDirectory path to tag directory 'tagDirs'
@@ -23,16 +27,16 @@ export function appendEntriesAndTags(event:IpcMainEvent, allEntries:string, tagD
     //if successful in creating child process..
     if (childProcess) 
     {
-      try
+      try 
       {
-
+        //log the childProcess
         childProcess.stderr?.on('data', (data) => {
           console.log('data:',data)
         })
         childProcess.on('error', (error) => {
           throw error
         })
-        childProcess.on('exit', (code,signal) => {
+        childProcess.on('exit', (code, signal) => {
           printFormatted('yellow', 'exit code: ', code, '\nsignal:',signal)
         })
         //reply to the event with...
