2aa843e611ac59c9d7956d50d3e2ada421aafc0b
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-05-07 18:12:50 +0200

    Update settings.ts

--- ts/view/settings.ts
+++ ts/view/settings.ts
@@ -1,6 +1,7 @@
 import { ipcRenderer } from 'electron';
 import { activate } from './load-themes';
-import { setPasswordProtection } from './switch/password-switch'; 
+import { checkPasswordProtection, setPasswordProtection } from './switch/password-switch'; 
+import { loadRegisterEmailPasswordDialog } from './fragments/load-fragments';
 
 //SECTION - Theme Buttons
 /** Constants */ //these are relative to the html page 'settings.html'
@@ -17,25 +18,28 @@ var btn_leafy_theme = document.querySelector('#btn-leafy-theme') as HTMLDivEleme
 
 //DEFAULT BUTTON
 btn_default_theme ?  
-enableButton(btn_default_theme,DEFAULT_THEME) :
+enableThemeButton(btn_default_theme,DEFAULT_THEME) :
 console.warn('btn_default_theme is null')
 
 
 //DARK THEME BUTTON
 btn_dark_theme ?
-enableButton(btn_dark_theme,DARK_THEME) :
+enableThemeButton(btn_dark_theme,DARK_THEME) :
 console.warn('btn_dark_theme is null')
 
 
 //LEAFY THEME BUTTON
 btn_leafy_theme ?
-enableButton(btn_leafy_theme,LEAFY_THEME) :
+enableThemeButton(btn_leafy_theme,LEAFY_THEME) :
 console.warn('btn_leafy_theme is null')
 
-/** Function to set buttons onclick with
- * activate function 
+/** 
+ * Function to set theme buttons onclick with
+ * activate function.
+ * Note: the activate function (inside this function)
+ * sets the theme of the app.
 */
-function enableButton(button:HTMLDivElement, theme:string)
+function enableThemeButton(button:HTMLDivElement, theme:string)
 {
     button.onclick = () => activate(theme);
 }
@@ -43,8 +47,22 @@ function enableButton(button:HTMLDivElement, theme:string)
 
 
 //SECTION - Password Protection 
+checkPasswordProtection()//whether or not password protection switch should be checked
 
-
-
+const btn_password_protection_false = document.querySelector('#no-password') as HTMLDivElement
+const btn_password_protection_true = document.querySelector('#password-protection') as HTMLDivElement
+btn_password_protection_false.onclick = () =>  setPasswordProtection('false')
+btn_password_protection_true.onclick = () =>  { 
+    //set password protection to true if email is already 
+    if (emailIsVerified() && passwordIsSet()) {
+        setPasswordProtection('true') 
+    }
+    else {
+        //take user password input
+        loadRegisterEmailPasswordDialog()
+        saveEmailPasswordInput()
+        //NOTE:will verify the user password when they click the verify email button
+    }   
+}
 
 //SECTION - Verify Email Address
