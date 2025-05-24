0af0c397ce66a92d6c1840717e89b9a343c436cd
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-05-24 16:41:42 +0200

    email-password-checks.ts - add to comments

--- ts/view/switch/email-password-checks.ts
+++ ts/view/switch/email-password-checks.ts
@@ -1,6 +1,9 @@
 import { ipcRenderer } from 'electron'
 /**
  * Checks whether the email is verified.
+ * This is the frontend version of this function.
+ * There is a backend version of this function which
+ * checks the `email-verified.json` file directly.
  * @returns boolean 
  */
 export async function emailIsVerified(email:string):Promise<boolean> {
